7a696995be09009cf700eab516687dfdb8c9c538
Archon <den_archangel@mail.ru>
2016-12-23 15:41:49 +0500

    final 0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,3 @@
-/*"use strict";
-
-module.exports = function(grunt) {
-  grunt.loadNpmTasks("grunt-browser-sync");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-postcss");
-  grunt.loadNpmTasks("grunt-sass");
-
-  grunt.initConfig({
-    sass: {
-      style: {
-        files: {
-          "css/style.css": "sass/style.scss"
-        }
-      }
-    },
-
-    postcss: {
-      style: {
-        options: {
-          processors: [
-            require("autoprefixer")({browsers: [
-              "last 2 versions"
-            ]})
-          ]
-        },
-        src: "css!!.css"
-      }
-    },
-
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: [
-            "*.html",
-            "css !! .css"
-          ]
-        },
-        options: {
-          server: ".",
-          watchTask: true,
-          notify: false,
-          open: true,
-          cors: true,
-          ui: false
-        }
-      }
-    },
-
-    watch: {
-      style: {
-        files: ["sass.{scss !!! ,sass}"],
-        tasks: ["sass", "postcss"]
-      }
-    }
-  });
-  
-  grunt.registerTask("serve", ["browserSync", "watch"]);
-};*/
-
-
-
 module.exports = function(grunt) {
   
   require("load-grunt-tasks")(grunt);
@@ -68,7 +6,7 @@ module.exports = function(grunt) {
     sass: {
       style: {
         files: {
-          "build/css/style.css": ["sass/style.scss"]
+          "build/css/style.css": "sass/style.scss"
         }
       }
     },
@@ -98,7 +36,7 @@ module.exports = function(grunt) {
         tasks: ["copy:html"]
       },
       style: {
-        files: ["sass/** /*.scss"],
+        files: ["sass/**/*.{scss,sass}"],
         tasks: ["sass", "postcss", "csso"]
       }
     },
